e2e24de81e6bb8a699eb20852b5f75fb91210f03
André Ferreira <andreferreira.coder@gmail.com>
2020-05-20 21:04:11 +0100

    useContext

--- src/index.js
+++ src/index.js
@@ -3,5 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
+import AuthContext from './context/auth-context';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <AuthContext>
+        <App />
+    </AuthContext>,
+    document.getElementById('root'));
